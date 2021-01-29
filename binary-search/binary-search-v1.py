1571b65ccf18f3495c002f9fa2b8d338c55375b2
sassafras13 <benjaminsonemma9@gmail.com>
2021-01-28 23:10:49 -0500

    Add implementations of binary search (recursive and iterative)

--- binary-search/binary-search-v1.py
+++ binary-search/binary-search-v1.py
@@ -33,10 +33,33 @@ def find_index_key(elements, value, key):
         elif middle_element > value:
             right = middle - 1
 
+# recursive implementation
+def contains(elements, value):
+    left, right = 0, len(elements) - 1
+
+    if left <= right:
+        middle = (left + right) // 2
+
+        if elements[middle] == value:
+            return True
+
+        if elements[middle] < value: 
+            return contains(elements[middle + 1:], value) 
+        elif elements[middle] > value: 
+            return contains(elements[:middle], value)
+
+    return False
+
 # driver code
+
 fruits = ['orange', 'plum', 'watermelon', 'apple']
-print(fruits.sort(key=len))
+print(fruits)
+fruits.sort(key=len) # sorts the list in ascending order by the length of the word
+print(fruits)
 
 print(fruits[find_index_key(fruits, key=len, value=10)])
 
 print(find_index_key(fruits, key=len, value=3))
+
+myList = [1, 3, 65, 135, 342]
+print(contains(myList, value=342))
